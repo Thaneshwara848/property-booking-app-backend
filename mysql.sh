6b2336234eef754f3f2eec0854d6ee45b71afdca
Arunvel Arunachalam <aryan.arunachalam@gmail.com>
2025-09-18 16:58:36 +0530

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -14,15 +14,9 @@ sudo systemctl start mysql
 echo "Creating database and user..."
 sudo mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS basic_tutorial;
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON basic_tutorial.* TO 'root'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON basic_tutorial.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
 EOF
-
-echo "Configuring MySQL to listen on all interfaces..."
-sudo sed -i "s/^bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
-
-echo "Restarting MySQL..."
-sudo systemctl restart mysql
 
 echo "MySQL setup completed!"
